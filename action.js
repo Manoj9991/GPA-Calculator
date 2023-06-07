5cda87fb18dd88c821980806bfee09b174a486d3
Manoj Lakshan <92544384+Manoj9991@users.noreply.github.com>
2023-06-07 03:44:19 -0700

    Update action.js

--- action.js
+++ action.js
@@ -1,1 +1,28 @@
+function calculateGPA() {
+    var totalCredits = 0;
+    var totalGradePoints = 0;
 
+    var table = document.getElementById("table2");
+    var rows = table.getElementsByTagName("tr");
+
+    for(var i=0; i < rows.length-1; i++){
+        var row = rows[i];
+        var credit = parseFloat(row.querySelector(".grade").value);
+        console.log(credit);
+        //var credit = parseFloat(row.document.getElementById())
+        
+        var grade = parseFloat(row.querySelector("select").value);
+        console.log(grade);
+        if(!isNaN(credit) && !isNaN(grade)) {
+            totalCredits += credit;
+            totalGradePoints += credit * grade;
+        }
+    }
+    console.log(totalCredits);
+    console.log(totalGradePoints);
+    var gpa = totalGradePoints / totalCredits;
+    console.log(gpa);
+    if(!isNaN(gpa)){
+        document.getElementById("result").textContent = gpa.toFixed(2);
+    }
+}
